9c049a15934dbd93530057ed89d6b9edcc5f834b
Kevin Fei <kev1fei@gmail.com>
2017-09-16 19:41:14 -0400

    parsing the wolfram url

--- plot_xy.py
+++ plot_xy.py
@@ -4,15 +4,38 @@
 # At each point, point and turn.
 
 import math
-import create2cli
+#import create2cli
+import urllib
 
-SCALING_FACTOR = 1
+MAGIC_URL = 'https://www.wolframcloud.com/objects/user-0bd94b55-72c1-466a-8c79-c0ad96a59807/output?y=1'
 
 class RoombaNavigate():
     position = (0,0)    #in units of scaling
     theta = 0           #radians from x-axis
     def __init__(self):
         print 'start'
+
+    def download_points(self):
+        try:
+            file_handler = urllib.urlopen(MAGIC_URL)
+        except IOError:
+            print 'Connection Error: could not connect'
+        data = file_handler.readline()
+        wolfram_string = data[:-6]
+        end_str = wolfram_string.rfind('}}', 2)
+        points_string = wolfram_string[3:end_str + 1]
+        points_array = points_string.split('}, {')
+        request_time = points_array.pop()
+        points = []
+        maxy = float('-inf')
+        for point in points_array:
+            points.append(point.split(', ').map(int))
+            if (points[1] > maxy):
+                maxy = points[1]
+        for i in range(len(points)):
+            points[i][1] *= SCALING_FACTOR / maxy
+        print points
+        return points
 
     def move_to_origin(self):
         print 'idk'
@@ -31,10 +54,12 @@ class RoombaNavigate():
         dist = math.sqrt((x - lastx) ** 2 + (y - lasty) ** 2)
         self.travel(dist)
 
-    def rotate
+    def rotate(angle):
+        print 'rotate ' + angle
 
     def travel(dist):
-
+        print 'travel ' + dist
 
 if __name__ == "__main__":
     roomba = RoombaNavigate()
+    roomba.download_points()
