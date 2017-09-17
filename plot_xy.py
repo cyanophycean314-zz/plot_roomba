c34dd6de8da42e152a0e979b77c55ef34b7383fa
Suproteem <suproteemsarkar@college.harvard.edu>
2017-09-16 20:07:38 -0400

    implemented helper functions for move

--- plot_xy.py
+++ plot_xy.py
@@ -4,14 +4,17 @@
 # At each point, point and turn.
 
 import math
-#import create2cli
+import create2_cli
 import urllib
+import time
 
 MAGIC_URL = 'https://www.wolframcloud.com/objects/user-0bd94b55-72c1-466a-8c79-c0ad96a59807/output?y=1'
 
 class RoombaNavigate():
-    position = (0,0)    #in units of scaling
-    theta = 0           #radians from x-axis
+    position = (0,0)            #in units of scaling
+    theta = 0                   #radians from x-axis
+    seconds_per_degree = 0.024  #found experimentally
+    seconds_per_dist_unit = 0.5   #tuned for best experience
     def __init__(self):
         print 'start'
 
@@ -49,17 +52,36 @@ class RoombaNavigate():
 
     def move(lastx, lasty, x, y):
         new_theta = math.atan2(y, x)
-        diff_theta = new_theta - new_theta
+        diff_theta = self.theta - new_theta
         self.rotate(diff_theta)
         dist = math.sqrt((x - lastx) ** 2 + (y - lasty) ** 2)
         self.travel(dist)
 
     def rotate(angle):
-        print 'rotate ' + angle
+        print 'rotate' + angle
+        degrees = math.degrees(angle)
+        print 'rotate ' + degrees + ' degrees'
+        if degrees <= 180:
+            app.sendCommandASCII('137 0 80 0 1')
+            time.sleep(degrees * this.seconds_per_degree)
+            app.sendCommandASCII('145 0 0 0 0')
+        else:
+            app.sendCommandASCII('137 0 80 255 255')
+            time.sleep((360 - degrees) * this.seconds_per_degree)
+            app.sendCommandASCII('145 0 0 0 0')
+
+    def travel(dist):
+        app.sendCommandASCII('145 0 100 0 100')
+        time.sleep(dist * this.seconds_per_dist_unit)
+        app.sendCommandASCII('145 0 0 0 0')
 
     def travel(dist):
         print 'travel ' + dist
 
 if __name__ == "__main__":
     roomba = RoombaNavigate()
-    roomba.download_points()
+    app = create2_cli.TetheredDriveApp()
+    # roomba.download_points()
+    app.sendCommandASCII('137 0 80 0 1')
+    time.sleep(1)
+    app.sendCommandASCII('145 0 0 0 0')
